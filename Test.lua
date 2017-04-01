efc9269c4035e677076f21d46a859676753c03ef
asbj1477 <asbj1477@gmail.com>
2017-04-01 16:31:49 +0200

    Update Test.lua

--- Test.lua
+++ Test.lua
@@ -1,10 +1,4 @@
 local  Garen = GetMyHero()
 if GetObjectName(Garen) ~= "Garen" then return end
-
-
-local Skins {
-	["Garen" = {"Classic", "Sanguine", "Desert Trooper", "Commando", "Dreadknight", "Rugged", "Steel Legion", "Chroma Pack: Garnet", "Chroma Pack: Plum", "Chroma Pack: Ivory", "Rogue Admiral"},
-}
 HeroSkinChanger(Garenm Sanguine)
-
 PrintChat("Hello, welcome to the test script :D")
